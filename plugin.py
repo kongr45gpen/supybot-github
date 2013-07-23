17396f73a99a717f896fc3609de5529b120af97f
kongr45gpen <kongr45gpen@helit.org>
2013-07-23 18:53:20 +0300

    Initial commit

---
+++ plugin.py
@@ -0,0 +1,19 @@
+###
+
+import supybot.utils as utils
+from supybot.commands import *
+import supybot.plugins as plugins
+import supybot.ircutils as ircutils
+import supybot.callbacks as callbacks
+
+
+class Github(callbacks.Plugin):
+    """Add the help for "@plugin help Github" here
+    This should describe *how* to use this plugin."""
+    pass
+
+
+Class = Github
+
+
+# vim:set shiftwidth=4 softtabstop=4 expandtab textwidth=79:
---
+++ test.py
@@ -0,0 +1,9 @@
+###
+
+from supybot.test import *
+
+class GithubTestCase(PluginTestCase):
+    plugins = ('Github',)
+
+
+# vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
